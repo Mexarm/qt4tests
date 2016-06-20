fe6ba4c84ab45d17c2a501f558ad902608175489
Mexarm <mexarm@me.com>
2016-06-19 22:49:34 -0500

    complete pyqtdesigner example

--- pyqtdesigner_main.py
+++ pyqtdesigner_main.py
@@ -1,11 +1,28 @@
-from PyQt4 import QtCore, QtGui
-from pyqtdesigner_ui import *
+from PyQt4 import QtGui
+import sys
 
-if __name__ == "__main__":
-    import sys
-    app = QtGui.QApplication(sys.argv)
-    MainWindow = QtGui.QMainWindow()
-    ui = Ui_MainWindow()
-    ui.setupUi(MainWindow)
-    MainWindow.show()
-    sys.exit(app.exec_())
+import pyqtdesigner_ui
+              
+
+class ExampleApp(QtGui.QMainWindow, pyqtdesigner_ui.Ui_MainWindow):
+    def __init__(self):
+        super(self.__class__, self).__init__()
+        self.setupUi(self)
+
+        self.pushButton.clicked.connect(self.btn_action)
+
+    def btn_action(self):
+        if self.radioButton.isChecked():
+            self.radioButton.setChecked(False)
+        else:    
+            self.radioButton.setChecked(True)
+        
+def main():
+    app = QtGui.QApplication(sys.argv) 
+    form = ExampleApp()                
+    form.show()                        
+    sys.exit(app.exec_())              
+
+
+if __name__ == '__main__':             
+    main()                             
